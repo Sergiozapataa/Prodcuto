8005f23d1e49efc04ed5ade486814052c2fde327
Sergiozapataa <165798740+Sergiozapataa@users.noreply.github.com>
2025-06-11 04:18:18 -0500

    Update database.js

--- src/services/database.js
+++ src/services/database.js
@@ -1,10 +1,5 @@
 export let usuarios = [
-    {
-        id: 1,
-        nombre: "Jaime Zapata",
-        usuario: "admin",
-        contrasena: "admin"
-    },
+   
     {
         id: 2,
         nombre: "Camila Perez",
